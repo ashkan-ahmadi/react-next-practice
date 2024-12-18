95f7b8792f821d6ba1dc062a6b7504fbb32af01d
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2024-12-19 00:25:42 +0100

    Simplify page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,12 @@ import './bs.css' // Bootstrap v5.3.3 CSS
 import UserInfoByID from './UserInfoByID/UserInfoByID'
 
 export default function Home() {
-  // Use Effect runs one time when this component loads
-  // Then it never ever runs even if you have state in it changing
-  // Unless the useEffect also has the state as a dependency
+  // const [state, setState] = useState()
   // useEffect(() => {}, [])
 
   return (
     <div className="container">
-      <h1>Hooks</h1>
+      <h1>React and Nextjs Practice</h1>
       <hr />
 
       <UserInfoByID />
